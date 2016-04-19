88822e3a6a414fec509b0abbea45e3142bc1bd1b
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-04-18 22:36:36 -0600

    commit

--- app.js
+++ app.js
@@ -3,17 +3,35 @@ function getMusic(){
   var artist = document.getElementById('artist').value;
   itunes.getMusicByArtist(artist).then(drawSongs);
 }
+function reset(){
+    var songListElem= '';
+}
+var songListElem = document.getElementById('songList');
+
 
 function drawSongs(songList){
-  
-  var songListElem = document.getElementById('songList');
-for(var i =0; i< songList.length; i++){
+  reset();
+for(var i =0; i < songList.length; i++){
   var currentSong = songList[i];
   
-songListElem.innerHTML +=  '<br>'+'<li>' + "Title: "+ currentSong.title  + "<li>" + " Album Art: " + '<img src=' +currentSong.albumArt + '>' + "<li>" + " Collection: " + currentSong.collection + "<li>" + "Price: $" + currentSong.price + "<li>" + " Preview: " + '<audio src=' +currentSong.preview + ' controls><audio>'+'>' + '<br>';
-}
-}
+// songListElem.innerHTML += '<li class="list-group-item"><div class="row image-responsive">'+
 
-// function reset(){
-//   songListElem.innerHTML= '';
+// '<div class="col-xs-4"><img src="' + currentSong.albumArt + '"></div>'+
+// '<div class="col-xs-4"><h4>Title: '+currentSong.title  + '</h4>'
+// + currentSong.collection + '<br> $' + currentSong.price + '</div>' +
+// '<div class="col-xs-4"><audio src="' +currentSong.preview +  '"controls><audio></div></li>'
 // }
+// }
+
+
+
+songListElem.innerHTML += 
+'<li class="list-group-item>'+
+'<h4>'+ currentSong.title  + '</h4>'+
+'<p><img src="' + currentSong.albumArt + '"></p>'+
+'<p><audio src="' +currentSong.preview +  '"controls><audio></p>'+ 
+'<p>'+ currentSong.collection + '</p>' +
+'<p> $' + currentSong.price + '</p>' +
+'</li>'
+}
+}
